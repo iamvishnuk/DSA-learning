e1dba4472eae0e1f359346890f6b993d9573a643
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-02-15 11:20:20 +0530

    feat: leetcode 67

---
+++ LeetCode/Easy/67.ts
@@ -0,0 +1,21 @@
+// 67. Add Binary
+
+function addBinary(a: string, b: string): string {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  let result = "";
+
+  while (i >= 0 || j >= 0 || carry) {
+    let sum = carry
+
+    if (i >= 0) sum += Number(a[i--])
+    if (j >= 0) sum += Number(b[j--])
+
+    result = (sum % 2) + result
+    carry = Math.floor(sum / 2)
+  }
+
+  return result
+
+};
