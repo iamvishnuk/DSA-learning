68fc5c3516f471b1c4da453b4a3f9e2a97edd0ad
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-02-18 22:56:49 +0530

    feat: leetcode 693

---
+++ LeetCode/Easy/693.ts
@@ -0,0 +1,15 @@
+// 693. Binary Number with Alternating Bits
+
+function hasAlternatingBits(n: number): boolean {
+  let prev = n & 1;
+  n = n >> 1
+
+  while (n > 0) {
+    let curr = n & 1;
+    if (curr === prev) return false;
+    prev = curr;
+    n = n >> 1
+  }
+
+  return true
+};
