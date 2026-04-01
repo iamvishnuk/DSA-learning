44616334b9a197ebe0585524e78ac387903902bc
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-04-01 10:01:08 +0530

    feat: leetcode 122

---
+++ LeetCode/122.ts
@@ -0,0 +1,18 @@
+// 122. Best Time to Buy and Sell Stock II
+/*
+ * Array
+ * Dynamic Programming
+ * Greedy
+ */
+
+function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      maxProfit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return maxProfit;
+}
