684b155d822324b90ef06870ad74bc9d8e1a9072
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-04-11 10:10:43 +0530

    feat: leetcode 3741

---
+++ LeetCode/Medium/3741.ts
@@ -0,0 +1,28 @@
+//  3741. Minimum Distance Between Three Equal Elements II
+/*
+ * Array
+ * Hash Table
+ */
+
+function minimumDistance(nums: number[]): number {
+  const map = new Map<number, number[]>();
+  let minDist = Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map.has(nums[i])) {
+      map.set(nums[i], []);
+    }
+    map.get(nums[i])!.push(i);
+  }
+
+  for (const indices of map.values()) {
+    if (indices.length < 3) continue;
+
+    for (let i = 0; i <= indices.length - 3; i++) {
+      let dist = 2 * (indices[i + 2] - indices[i]);
+      minDist = Math.min(minDist, dist);
+    }
+  }
+
+  return minDist === Infinity ? -1 : minDist;
+}
