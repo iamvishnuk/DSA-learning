3757112b289db914725fdcffd5f50e597eb81351
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-05-16 09:05:10 +0530

    refactor: leetcode 154 js to ts

--- LeetCode/Hard/154.ts
+++ LeetCode/Hard/154.ts
@@ -1,18 +1,16 @@
 // 154. Find Minimum in Rotated Sorted Array II
-
-/**
- * @param {number[]} nums
- * @return {number}
+/*
+ * Array
+ * Binary Search
  */
-var findMin = function (nums) {
-  if (nums[0] < nums[nums.length - 1]) {
-    return nums[0];
-  }
 
+function findMin(nums: number[]): number {
   let start = 0;
   let end = nums.length - 1;
+
   while (start < end) {
     let mid = Math.floor((start + end) / 2);
+
     if (nums[mid] > nums[end]) {
       start = mid + 1;
     } else if (nums[mid] < nums[end]) {
@@ -23,7 +21,4 @@ var findMin = function (nums) {
   }
 
   return nums[end];
-};
-
-const nums = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 0, 1, 1, 1, 1, 1, 1];
-console.log(findMin(nums));
+}
