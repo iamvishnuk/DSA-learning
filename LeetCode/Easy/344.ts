41ae141c5184cfae1e0cd30dbf6409f024d67c61
iamvishnuk <vishnukkakkarayil@gmail.com>
2026-07-01 00:36:09 +0530

    feat: leetcode 344

---
+++ LeetCode/Easy/344.ts
@@ -0,0 +1,16 @@
+// 344. Reverse String
+/*
+ * Two Pointer
+ * String
+ */
+
+function reverseString(s: string[]): void {
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start <= end) {
+    [s[start], s[end]] = [s[end], s[start]];
+    start++;
+    end--;
+  }
+}
