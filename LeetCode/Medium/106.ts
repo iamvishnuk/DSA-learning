04e5b8d7c11a95b2902a08938347222a72b634ad
iamvishnuk <vishnukkakkarayil@gmail.com>
2026-07-10 23:05:29 +0530

    feat(LeetCode): 106 add solution using map

--- LeetCode/Medium/106.ts
+++ LeetCode/Medium/106.ts
@@ -1,8 +1,15 @@
 // 106. Construct Binary Tree from Inorder and Postorder Traversal
+/*
+ * Array
+ * Hash Table
+ * Divide and Conquer
+ * Tree
+ * Binary Tree
+ */
 
 import { TreeNode } from '../../types';
 
-function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+function buildTree1(inorder: number[], postorder: number[]): TreeNode | null {
   const map = new Map();
   inorder.forEach((v, i) => map.set(v, i));
 
@@ -32,4 +39,30 @@ function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
   }
 
   return recurse(0, postorder.length - 1, 0, inorder.length - 1);
+}
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < inorder.length; i++) {
+    map.set(inorder[i], i);
+  }
+
+  let postIndex: number = postorder.length - 1;
+
+  const build = (left: number, right: number): TreeNode | null => {
+    if (left > right) return null;
+
+    const rootValue: number = postorder[postIndex--];
+    const mid: number = map.get(rootValue)!;
+
+    const root = new TreeNode(rootValue);
+
+    root.right = build(mid + 1, right);
+    root.left = build(left, mid - 1);
+
+    return root;
+  };
+
+  return build(0, inorder.length - 1);
 }
