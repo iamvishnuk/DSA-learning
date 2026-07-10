bf6ee8c6cfda34326bb814c2b900dcaa707a2c61
iamvishnuk <vishnukkakkarayil@gmail.com>
2026-07-10 23:56:13 +0530

    feat(LeetCode): 951

---
+++ LeetCode/Medium/951.ts
@@ -0,0 +1,22 @@
+// 951. Flip Equivalent Binary Trees
+/*
+ * Tree
+ * Depth-First Search
+ * Binary Tree
+ */
+
+import { TreeNode } from '../../types';
+
+function flipEquiv(root1: TreeNode | null, root2: TreeNode | null): boolean {
+  if (!root1 && !root2) return true;
+
+  if (!root1 || !root2) return false;
+
+  if (root1.val !== root2.val) return false;
+
+  return (
+    (flipEquiv(root1.left, root2.left) &&
+      flipEquiv(root1.right, root2.right)) ||
+    (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+  );
+}
